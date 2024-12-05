2950993afb37270486e439bf0a06c9862408bbc5
George Boktor <gsboktor@gmail.com>
2024-12-05 17:50:31 -0600

    app gen and basic configs

---
+++ dist/packages/boktor-portfolio/styles.js
@@ -0,0 +1,17 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([[869],{
+
+/***/ 27:
+/***/ (() => {
+
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(27));
+/******/ }
+]);
---
+++ packages/boktor-portfolio/src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./app.module.css";
+
+export function App() {
+  return (
+    <div>
+      <div className={styles.poo}>Testing 123</div>
+    </div>
+  );
+}
+
+export default App;
