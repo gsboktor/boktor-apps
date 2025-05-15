1cdf4ac5013a7d67d75310c924a8ef45fd22360c
George Boktor <gsboktor@gmail.com>
2025-05-15 13:25:03 -0500

    nest-js-boilerplate and config

---
+++ packages/lio-services/webpack.config.js
@@ -0,0 +1,21 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/packages/lio-services'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: true,
+      outputHashing: 'none',
+      sourceMap: false,
+      generatePackageJson: false,
+    }),
+  ],
+};
